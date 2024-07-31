72e390543567e9a3c9f18d2df4098ca5b30215f8
Luxcas213 <145593132+luxcas213@users.noreply.github.com>
2024-07-31 16:48:28 -0300

    jhasdf

--- modelo/matrix.py
+++ modelo/matrix.py
@@ -1,6 +1,6 @@
-n=10
+n=3
 matrix = [[[1 for _ in range(n)] for _ in range(n)] for _ in range(n)]
-
+""" 
 
 xy = [[1 if (i > 2 and i < 7 and j > 2 and j < 7) else 0 for j in range(n)] for i in range(n)]
 
@@ -8,8 +8,15 @@ xz = [[1 if (i > 2 and i < 7 and j > 2 and j < 7) else 0 for j in range(n)] for 
 
 yz = [[1 if (i > 2 and i < 7 and j > 2 and j < 7) else 0 for j in range(n)] for i in range(n)]
 
+ """
+xy = [[1,1,1],
+      [1,0,1],
+      [1,1,1]]
 
-xy[0][0]=1
-xz[0][0]=1
-yz[0][0]=1
+xz = [[1,1,1],
+      [1,0,1],
+      [1,1,1]]
 
+yz = [[1,1,1],
+      [1,0,1],
+      [1,1,1]]
