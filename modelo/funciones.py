361b056680788080928b874561a51307b3686982
Luxcas213 <lgarbate@gmail.com>
2024-08-12 15:42:43 -0300

    aa el renacimiento
    hice el codigomas legible y pase todo ordenado

--- modelo/funciones.py
+++ modelo/funciones.py
@@ -1,9 +1,15 @@
 import numpy as np
 from stl import mesh
 import os
+import numpy as np
+from typing import List
+import copy  
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
-def create_voxel_mesh(x, y, z, size_x,size_y,size_z):
-    #creo los vertices desde la pocicion inicial hatsa el tamaño de el recrangulo
+
+def create_voxel_mesh(x: int, y: int, z: int, size_x: int, size_y: int, size_z: int) -> tuple[np.ndarray, np.ndarray]:
+    # creo los vertices desde la pocicion inicial hasta el tamaño del rectángulo
     vertices = np.array([
         [x, y, z],
         [x + size_x, y, z],
@@ -15,7 +21,7 @@ def create_voxel_mesh(x, y, z, size_x,size_y,size_z):
         [x, y + size_y, z + size_z]
     ])
     
-    #asigno que trios de vertices generan un triangulo de cara
+    # asigno qué trios de vertices generan un triangulo de cara
     faces = np.array([
         [2, 1, 0],
         [3, 2, 0],
@@ -33,25 +39,25 @@ def create_voxel_mesh(x, y, z, size_x,size_y,size_z):
     
     return vertices, faces
 
-def voxel_to_mesh(voxel_matrix):
+def voxel_to_mesh(voxel_matrix: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
     
-    """"
-    uso el gready meshing para simplificar la matrix de voxeles y generar un mesh
+    """
+    Uso el gready meshing para simplificar la matriz de voxeles y generar un mesh
     """
     voxel_matrix = np.array(voxel_matrix, dtype=bool)
     
-    voxel_procesed = np.zeros_like(voxel_matrix,dtype=bool)
+    voxel_procesed = np.zeros_like(voxel_matrix, dtype=bool)
     all_vertices = []
     all_faces = []
-    n=voxel_matrix.shape[0]
+    n = voxel_matrix.shape[0]
     for x in range(n):
         for y in range(n):
             for z in range(n):
                 if voxel_matrix[x, y, z] and not voxel_procesed[x, y, z]:
-                    print(f'Processing voxel at ({x}, {y}, {z})')
+                    #print(f'Processing voxel at ({x}, {y}, {z})')
                     voxel_procesed[x][y][z] = True
                     size_x = size_y = size_z = 1
-                    print(f'size_x: {size_x}, size_y: {size_y}, size_z: {size_z}')
+                    #print(f'size_x: {size_x}, size_y: {size_y}, size_z: {size_z}')
                     for i in range(x + 1, n):
                         if not voxel_matrix[i, y, z] or voxel_procesed[i, y, z]:
                             break
@@ -70,7 +76,7 @@ def voxel_to_mesh(voxel_matrix):
                         voxel_procesed[x:x + size_x, y:y + size_y, k] = True
                         voxel_matrix[x:x + size_x, y:y + size_y, k] = 0
                         size_z += 1
-                    print(f'Voxel at ({x}, {y}, {z}) with size ({size_x}, {size_y}, {size_z})')
+                    #print(f'Voxel at ({x}, {y}, {z}) with size ({size_x}, {size_y}, {size_z})')
                     voxel_vertices, voxel_faces = create_voxel_mesh(x, y, z, size_x, size_y, size_z)
                     vertex_offset = len(all_vertices)
                     all_vertices.extend(voxel_vertices)
@@ -82,29 +88,30 @@ def voxel_to_mesh(voxel_matrix):
     all_vertices = np.array(all_vertices)
     
     return all_vertices, all_faces
-def armodoscubos():
-    all_vertices = []
-    all_faces = []
-    voxel_vertices, voxel_faces = create_voxel_mesh(0,0,0,3,2,2)
-    vertex_offset = len(all_vertices)
-    all_vertices.extend(voxel_vertices)
-    
-    for face in voxel_faces:
-        all_faces.append(face + vertex_offset)
-    
-    voxel_vertices, voxel_faces = create_voxel_mesh(3,0,0,1,3,2)
-    vertex_offset = len(all_vertices)
-    all_vertices.extend(voxel_vertices)
-    
-    for face in voxel_faces:
-        all_faces.append(face + vertex_offset)
-    
-    
-    all_faces = np.array(all_faces)
-    all_vertices = np.array(all_vertices)
-    save_mesh(all_vertices, all_faces, 'cubos.stl')
 
-def save_mesh(vertices, faces, filename):
+def createMatrix(xy: List[List[int]], xz: List[List[int]], yz: List[List[int]], n: int) -> List[List[List[int]]]:
+    matrix = [[[1 for _ in range(n)] for _ in range(n)] for _ in range(n)]
+    for x in range(n):
+        for y in range(n):
+            if xy[x][y] == 0:
+                for z in range(n):
+                    matrix[x][y][z] = 0
+
+    for x in range(n):
+        for z in range(n):
+            if xz[x][z] == 0:
+                for y in range(n):
+                    matrix[x][y][z] = 0
+
+    for y in range(n):
+        for z in range(n):
+            if yz[y][z] == 0:
+                for x in range(n):
+                    matrix[x][y][z] = 0
+    return matrix
+
+
+def save_mesh(vertices: np.ndarray, faces: np.ndarray, filename: str) -> None:
     #armo el mesh
     mesh_data = mesh.Mesh(np.zeros(faces.shape[0], dtype=mesh.Mesh.dtype))
     
@@ -120,5 +127,46 @@ def save_mesh(vertices, faces, filename):
     print(f'Mesh saved to {full_path}')
 
 
-if __name__ == '__main__':
-    armodoscubos()
+def tieneVecinoCero(x, y, z, matrix):
+    n = len(matrix)
+    direcciones = [(1, 0, 0), (-1, 0, 0), (0, 1, 0), (0, -1, 0), (0, 0, 1), (0, 0, -1)]
+    
+    for dx, dy, dz in direcciones:
+        auxX = x + dx
+        auxY = y + dy
+        auxZ = z + dz
+        if 0 <= auxX < n and 0 <= auxY < n and 0 <= auxZ < n:
+            if matrix[auxX][auxY][auxZ] == 0:
+                return True
+        else:
+            return True
+    return False
+
+def optimizarMatrix(matrix):
+    n = len(matrix)
+    matrixAUX = copy.deepcopy(matrix)
+    for x in range(n):
+        for y in range(n):
+            for z in range(n):
+                if matrix[x][y][z] == 1:
+                    if not tieneVecinoCero(x, y, z, matrix):
+                        matrixAUX[x][y][z] = 0
+    return matrixAUX
+
+
+def mostrar_voxeles(matrix, umbral=0.5):
+    matrix = np.array(matrix)
+    
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+   
+    voxeles = matrix 
+
+    ax.voxels(voxeles, edgecolor='k')
+
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+
+    plt.show()
--- modelo/index.py
+++ modelo/index.py
@@ -1,64 +1,16 @@
-from modelo import mostrar
 from modelo import matrix as mx
-from modelo import save_stl as save
-import copy  
-
-def tieneVecinoCero(x, y, z, matrix):
-    n = len(matrix)
-    direcciones = [(1, 0, 0), (-1, 0, 0), (0, 1, 0), (0, -1, 0), (0, 0, 1), (0, 0, -1)]
-    
-    for dx, dy, dz in direcciones:
-        auxX = x + dx
-        auxY = y + dy
-        auxZ = z + dz
-        if 0 <= auxX < n and 0 <= auxY < n and 0 <= auxZ < n:
-            if matrix[auxX][auxY][auxZ] == 0:
-                return True
-        else:
-            return True
-    return False
-
-def optimizarMatrix(matrix):
-    n = len(matrix)
-    matrixAUX = copy.deepcopy(matrix)
-    for x in range(n):
-        for y in range(n):
-            for z in range(n):
-                if matrix[x][y][z] == 1:
-                    if not tieneVecinoCero(x, y, z, matrix):
-                        matrixAUX[x][y][z] = 0
-    return matrixAUX
-
+from modelo import funciones as fn
 def main():
-    matrix = mx.matrix
     xy = mx.xy
     xz = mx.xz
     yz = mx.yz
     n = mx.n
-
-    for x in range(n):
-        for y in range(n):
-            if xy[x][y] == 0:
-                for z in range(n):
-                    matrix[x][y][z] = 0
-
-    for x in range(n):
-        for z in range(n):
-            if xz[x][z] == 0:
-                for y in range(n):
-                    matrix[x][y][z] = 0
-
-    for y in range(n):
-        for z in range(n):
-            if yz[y][z] == 0:
-                for x in range(n):
-                    matrix[x][y][z] = 0
-
     
-    #matrix = optimizarMatrix(matrix)
-    vertices, faces = save.voxel_to_mesh(matrix)
-    save.save_mesh(vertices, faces, 'output.stl')
-    #mostrar.mostrar_voxeles(matrix)
+    matrix=fn.createMatrix(xy, xz, yz, n)
+    #matrix = fn.optimizarMatrix(matrix)
+    vertices, faces = fn.voxel_to_mesh(matrix)
+    fn.save_mesh(vertices, faces, 'output.stl')
+    #fn.mostrar_voxeles(matrix)
     
 
 if __name__ == "__main__":
